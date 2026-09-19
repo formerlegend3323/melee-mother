de56083c2766c7209b85d07b903a02d10e352bf6
formerlegend3323 <medicalrock96@gmail.com>
2026-09-19 16:55:29 -0700

    Split animations bundle for CDN

--- dist/js/animations.js
+++ dist/js/animations.js
@@ -1,7 +1,29 @@
-Promise.all([
-  fetch('./animations.part.aa').then(r => r.text()),
-  fetch('./animations.part.ab').then(r => r.text())
-]).then(parts => {
-  const code = parts.join('');
-  (0, eval)(code);
-}).catch(console.error);
+(function() {
+  var base = new URL("./", document.currentScript.src);
+
+  Promise.all([
+    fetch(new URL("animations.part.aa", base)).then(r => r.arrayBuffer()),
+    fetch(new URL("animations.part.ab", base)).then(r => r.arrayBuffer())
+  ]).then(function(parts) {
+    var combined = new Uint8Array(parts[0].byteLength + parts[1].byteLength);
+
+    combined.set(new Uint8Array(parts[0]), 0);
+    combined.set(new Uint8Array(parts[1]), parts[0].byteLength);
+
+    var blob = new Blob([combined], {
+      type: "application/javascript"
+    });
+
+    var script = document.createElement("script");
+    script.src = URL.createObjectURL(blob);
+
+    script.onload = function() {
+      URL.revokeObjectURL(script.src);
+      window.meleeAnimationsLoaded = true;
+    };
+
+    document.head.appendChild(script);
+  }).catch(function(error) {
+    console.error("[ANIMATIONS] Failed to load:", error);
+  });
+})();
